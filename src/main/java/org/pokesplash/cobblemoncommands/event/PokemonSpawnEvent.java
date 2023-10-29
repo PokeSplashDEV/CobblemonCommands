71940e76b4ba13e3d09c316596d0d5fbc339ecff
bencrow11 <bencrow11@gmail.com>
2023-10-29 16:38:58 +0000

    HA can now be traced

--- src/main/java/org/pokesplash/cobblemoncommands/event/PokemonSpawnEvent.java
+++ src/main/java/org/pokesplash/cobblemoncommands/event/PokemonSpawnEvent.java
@@ -3,6 +3,7 @@ package org.pokesplash.cobblemoncommands.event;
 import com.cobblemon.mod.common.api.Priority;
 import com.cobblemon.mod.common.api.abilities.AbilityTemplate;
 import com.cobblemon.mod.common.api.events.CobblemonEvents;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import kotlin.Unit;
 import org.pokesplash.cobblemoncommands.CobblemonCommands;
 import org.pokesplash.cobblemoncommands.util.CobblemonUtils;
@@ -11,17 +12,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class PokemonSpawnEvent {
 	public void registerEvent() {
-		CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.NORMAL, e -> {
+		CobblemonEvents.POKEMON_ENTITY_SPAWN.subscribe(Priority.NORMAL, e -> {
 
 			if (CobblemonCommands.config.getHiddenAbility().isEnableSpawning()) {
 				int randomNumber = ThreadLocalRandom.current()
 						.nextInt(0, CobblemonCommands.config.getHiddenAbility().getSpawnRate());
 
 				if (randomNumber == 0) {
-					AbilityTemplate abilityTemplate = CobblemonUtils.getHA(e.getPokemon());
+					AbilityTemplate abilityTemplate = CobblemonUtils.getHA(e.getEntity().getPokemon());
+
+					Pokemon pokemon = e.getEntity().getPokemon();
 
 					if (abilityTemplate != null) {
-						e.getPokemon().setAbility(abilityTemplate.create(true));
+						pokemon.setAbility(abilityTemplate.create(true));
+
+						e.getEntity().setPokemon(pokemon);
 					}
 				}
 			}
